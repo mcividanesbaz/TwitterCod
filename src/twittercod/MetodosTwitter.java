fe3535475ebbb0088c3c5792d6e8d18d7e2f6278
mcividanesbaz <Navidad1995>
2016-04-07 20:48:30 +0200

    Creado metodo timeline

--- src/twittercod/MetodosTwitter.java
+++ src/twittercod/MetodosTwitter.java
@@ -1,7 +1,10 @@
 
 package twittercod;
 
+import java.util.List;
+import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
@@ -15,13 +18,22 @@ public class MetodosTwitter {
     
     
     public MetodosTwitter() {
-
         cb.setDebugEnabled(true)
                 .setOAuthConsumerKey("nMhoaCVcMj3wvhte8QnkxqxLM")
                 .setOAuthConsumerSecret("JdTAcQt5IsH3QC5ukTXSFhewcUTYNOZtn7m0c89nPAgw6IofHJ")
                 .setOAuthAccessToken("867802357-I3SKAeEOeyDT1E6DG4BjanhI07c2GXCIKtuYBwW1")
                 .setOAuthAccessTokenSecret("8gKsSZF1QZ1T7de0EEqrr7yNO2hvM6NrMS1ld0BbrWu5p");
         twitter = new TwitterFactory(cb.build()).getInstance();  
-     
     }
+    
+     public void TimeLine()throws TwitterException{        
+        List<Status> statuses = twitter.getHomeTimeline();
+            System.out.println("Showing home timeline.");
+            for (Status status : statuses) {
+                System.out.println(status.getUser().getName() + ":"
+                        + status.getText());
+            }
+    
+    
+}
 }

d70a7ec6d8d677131ff04b2a08a4c0d812c3d554
mcividanesbaz <Navidad1995>
2016-04-07 20:52:13 +0200

    Commit con el menu creado para elegir la accion a la que queremos acceder.

--- src/twittercod/TwitterCod.java
+++ src/twittercod/TwitterCod.java
@@ -5,6 +5,9 @@
  */
 package twittercod;
 
+import javax.swing.JOptionPane;
+import twitter4j.TwitterException;
+
 /**
  *
  * @author Marcos
@@ -14,8 +17,39 @@ public class TwitterCod {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws TwitterException {
+     
+        String busqueda;
+        String tweet;
+        int opcion;
+        
+        MetodosTwitter obj = new MetodosTwitter();
+        
+        
+        do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("opciones: \n1 --> TimeLine\n2 --> Buscar\n3 --> Tweettear\n4 --> Exit"));
+            switch (opcion) {
+                case 1:
+                    obj.TimeLine();
+                    break;
+                case 2:
+                    busqueda = JOptionPane.showInputDialog("Introduzca la palabra por la que desee buscar");
+                    obj.ShowString(busqueda);
+                    break;
+                case 3:
+                    tweet = JOptionPane.showInputDialog("Introduzca lo que desee twittear");
+                    obj.Twittear(tweet);
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.exit(0);
+            }
+        } while (opcion != 0 && opcion != 4);
+    }
+        
+        
+        
     }
     
 }
